12db8c48b0384a31e0f0be2bf68537218606311e
gallit0 <mateogesede@gmail.com>
2022-08-30 21:09:13 -0700

    WIP: task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,19 +11,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = 0;
 
+	if (!parent)
+		return (0);
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (0);
 	new->n = value;
-	if (parent && parent->left)
+	if (parent->left)
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
 	}
-	if (parent)
-	{
-		parent->left = new;
-		new->parent = parent;
-	}
+	parent->left = new;
+	new->parent = parent;
 	return (new);
 }
--- test.c
+++ test.c
@@ -9,14 +9,6 @@
  */
 int main(void)
 {
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-    binary_tree_delete(root);
-    return (0);
+	binary_tree_insert_left(NULL, 64);
+	return (0);
 }
