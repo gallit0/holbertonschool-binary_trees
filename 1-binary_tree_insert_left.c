4f972707c2b1c739cb43207d245f4c1f10dcf593
gallit0 <mateogesede@gmail.com>
2022-08-30 20:46:24 -0700

    task 0 fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = 0, *p = 0;
+	binary_tree_t *new = 00;
 
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
