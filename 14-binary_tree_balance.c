f124b5e53cf577c9c0c161631e592fbcc05a851f
gallit0 <mateogesede@gmail.com>
2022-09-01 14:38:24 -0700

    task 14 done

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * b_t_b - balance of tree
+ * @tree: binary tree
+ * Return: int
+ */
+
+int b_t_b(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	left = b_t_b(tree->left);
+	right = b_t_b(tree->right);
+
+	if (left > right)
+		return (right + 1);
+	return (left + 1);
+}
+
+/**
+ * binary_tree_balance - balance of tree
+ * @tree: binary tree
+ * Return: int
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+	l = b_t_b(tree->left);
+	r = b_t_b(tree->right);
+	if (l > r)
+		return (l);
+	return (r * -1);
+}
