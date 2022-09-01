49e6cae25b57335187df968230adea14e4a3dfea
gallit0 <mateogesede@gmail.com>
2022-09-01 09:41:24 -0700

    task 9 done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * b_t_h - function that measures the height
+ * @tree: binary tree
+ * Return: size_t
+ */
+
+size_t b_t_h(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (-1);
+
+	left = b_t_h(tree->left);
+	right = b_t_h(tree->right);
+
+	if (left > right)
+		return (left + 1);
+	return (right + 1);
+}
+
+/**
  * binary_tree_height - function that measures the height
  * @tree: binary tree
  * Return: size_t
@@ -8,16 +29,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	const binary_tree_t *p = 0;
-	size_t height = 0;
-
 	if (!tree)
 		return (0);
-	p = tree->right;
-	while (p)
-	{
-		height++;
-		p = p->right;
-	}
-	return (height);
+	return (b_t_h(tree));
 }
