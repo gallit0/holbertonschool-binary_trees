64f7ba61e3adf228f123546805533d0225e7ecdf
gallit0 <mateogesede@gmail.com>
2022-09-01 16:04:31 -0700

    task 16 done

---
+++ matiu/16-binary_tree_is_perfect.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if tree is perfect
+ * @tree: binary tree
+ * Return: int
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+}
