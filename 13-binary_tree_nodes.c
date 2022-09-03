8f2be85c898ac707b12629931175ed4b52166efb
Denisse Landau <denisselandau97@gmail.com>
2022-09-02 17:14:47 -0700

    num of nodes at least 1 child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts nodes with at least 1 child binary tree.
+ * @tree: tree to count.
+ * Return: num of nodes, or 0 upon failure.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->right || tree->left)
+		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
+}
