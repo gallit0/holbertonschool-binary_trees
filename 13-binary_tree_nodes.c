e5db883070c833bb4a7baf3e368e0c828604c470
Denisse Landau <denisselandau97@gmail.com>
2022-09-02 17:15:55 -0700

    num of nodes at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,4 +10,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	if (tree->right || tree->left)
 		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
+	return (0);
 }
